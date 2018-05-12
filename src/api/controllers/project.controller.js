c40dd097b80497f5cfd12f20c8148e0e0e9e0aba
Souvik Chakraborty <souvik.c5@tcs.com>
2018-05-12 20:53:20 +0530

    changes in mongorepository

--- src/api/controllers/project.controller.js
+++ src/api/controllers/project.controller.js
@@ -68,11 +68,12 @@ exports.contributor = async (req, res, next) => {
 exports.project = async (req, res, next) => {
     try {
         const { projectID } = req.params;
-        const isUserAllowed = await isUserAContributor(req.user._id, projectID);
-        if (isUserAllowed) {
+        console.log("projectid", projectID);
+        // const isUserAllowed = await isUserAContributor(req.user._id, projectID);
+        // if (isUserAllowed) {
             const project = await getProject(projectID);
             return res.status(httpStatus.OK).json(project);
-        }
+        // }
         return res.status(httpStatus.BAD_REQUEST).json({
             message: 'NOT A CONTRIBUTOR',
         });
